6b880328a962ecd4687ffa693680f22950a73aca
AIT-LAHCEN REDOUAN <93483454+AIT-LAHCEN-REDOUAN@users.noreply.github.com>
2026-05-06 11:39:45 +0100

    Add Github Pages , deploy

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
   export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: "/ait-lahcen-redouan/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
